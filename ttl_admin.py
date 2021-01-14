1227d111629165d5a52f373aa7efb9d0aecc3b50
v_haotchen <v_haotchen@tencent.com>
2021-01-14 17:37:23 +0800

    v1

---
+++ ttl_admin.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+
+class Admin_Entrance:
+
+    '''
+    管理员鉴权信息模块
+    自身self.ID同来辨别身份否正确
+    '''
+
+    def __init__(self):
+        self.ID = 'y'
+
+    # 验证管理员身份，并给出返回值
+    def admin_checkout(self):
+        admin_id = str(input('管理员登录* 验证身份ID >: ')).strip()
+        if admin_id.lower() == self.ID:
+            return True
+        if admin_id.lower() != self.ID:
+            return False
+
+    # 接受验证函数的返回值，可以通过result 判断身份验证是否成功或失败
+    def get_admin_check(self):
+        result = self.admin_checkout()
+        print(result)
+
+
+class Position:
+    '''
+    管理会员和超市职员
+    主要功能： 修改人员的相关信息
+    '''
+
+    def  __init__(self):
+        pass
+
+    def shop_vip(self):
+        pass
+
+    def shop_staff(self):
+        pass
+
+
+class After_server:
+    '''
+    售后管理
+    主要功能：退货、换货、退款等操作
+    '''
+
+    def __init__(self):
+        pass
+
+    def
---
+++ ttl_welcome.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+
+class Welcome:
+    def __init__(self):
+        pass
+
+    def wellcome(self):
+        print('='*45)
+        print(' \t-*-  欢迎使用米加超市业务系统  -*- ')
+        print('='*45)
